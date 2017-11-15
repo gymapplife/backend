8a91ec3e167fd2e802873ae09d026bdb1ec12fe4
Gary <garydzheng@gmail.com>
2017-11-14 20:19:45 -0500

    gymapplife-

--- backend/db_models/models/uploaded_photo.py
+++ backend/db_models/models/uploaded_photo.py
@@ -3,4 +3,4 @@ from db_models.models.base.abstract_uploaded_media import AbstractUploadedMedia
 
 class UploadedPhoto(AbstractUploadedMedia):
 
-    s3_bucket = 'uploaded-photo'
+    s3_bucket = 'gymapplife-uploaded-photo'
